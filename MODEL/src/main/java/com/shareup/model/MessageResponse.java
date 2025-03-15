763c680eddaf3b4fee5bd2758450948c57d05ede
Yonatan Eshed <yonatan.eshed@gmail.com>
2025-03-15 23:59:47 +0200

    refactor: modify BaseService to support List, Single or Message Responses

---
+++ MODEL/src/main/java/com/shareup/model/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.shareup.model;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
